456192c98d99bee48129c91af07d3cf9dd4ee63d
KJB <drv98@naver.com>
2026-08-18 09:34:46 +0900

    프로젝트 시작

---
+++ src/main/java/com/example/busanstamp/BusanstampApplication.java
@@ -0,0 +1,13 @@
+package com.example.busanstamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusanstampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusanstampApplication.class, args);
+	}
+
+}
