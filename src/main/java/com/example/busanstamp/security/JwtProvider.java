57b3ba5183b540182e410517ac51b0b156b46658
KJB <drv98@naver.com>
2026-08-18 12:38:49 +0900

    JWT 생성 및 검증

---
+++ src/main/java/com/example/busanstamp/security/JwtProvider.java
@@ -0,0 +1,57 @@
+package com.example.busanstamp.security;
+
+import com.example.busanstamp.user.User;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+
+    private final SecretKey secretKey;
+    private final long accessTokenExpiration;
+    // 생성자에서 시크릿키(문자열) ,유효기간을 입력받아서 토큰발행을 위한 서명용키로 변환
+    public JwtProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.access-token-expiration}")
+            long accessTokenExpiration
+    ) {
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+        this.accessTokenExpiration = accessTokenExpiration;
+    }
+    //jwt 토큰 발행 (유저 객체)
+    public String createAccessToken(User user) {
+        Instant now = Instant.now();
+        Instant expiration = now.plusMillis(accessTokenExpiration);
+
+        return Jwts.builder()
+                .subject(String.valueOf(user.getUserId()))
+                .claim("email", user.getEmail())
+                .claim("role", user.getRole())
+                .issuedAt(Date.from(now))
+                .expiration(Date.from(expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+    //토큰을 암호풀어서 유저정보를 지닌 Claims 객체 리턴
+    public Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+    //유효기간(ms => s 초로 변환)
+    public long getExpirationSeconds() {
+        return accessTokenExpiration / 1000;
+    }
+}
