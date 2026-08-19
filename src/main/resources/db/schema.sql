da7b2c2e42c27af93da403a96b8b214dcaaaefb1
KJB <drv98@naver.com>
2026-08-19 10:22:34 +0900

    2. 관광 장소 테이블 생성

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -14,3 +14,21 @@ CREATE TABLE users (
 
     CONSTRAINT uk_users_email UNIQUE (email)
 );
+
+CREATE TABLE places (
+    place_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    address VARCHAR(255) NOT NULL,
+    latitude DECIMAL(10, 7) NOT NULL,  -- 위도
+    longitude DECIMAL(10, 7) NOT NULL, -- 경도
+    category VARCHAR(50) NOT NULL,
+    image_url VARCHAR(500),      -- 이미지 주소
+    created_by BIGINT NOT NULL,  -- 외래키 users.user_id 참조함
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+        ON UPDATE CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_places_created_by
+        FOREIGN KEY (created_by) REFERENCES users(user_id)
+);
