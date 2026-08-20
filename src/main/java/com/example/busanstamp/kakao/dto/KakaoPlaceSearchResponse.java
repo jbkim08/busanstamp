d419d1b525b887508652b582317e11c79fe3d651
KJB <drv98@naver.com>
2026-08-20 09:36:25 +0900

    7. 카카오 원본 응답 DTO

---
+++ src/main/java/com/example/busanstamp/kakao/dto/KakaoPlaceSearchResponse.java
@@ -0,0 +1,63 @@
+package com.example.busanstamp.kakao.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record KakaoPlaceSearchResponse(
+        Meta meta,
+        List<Document> documents
+) {
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Meta(
+
+            @JsonProperty("total_count")
+            int totalCount,
+
+            @JsonProperty("pageable_count")
+            int pageableCount,
+
+            @JsonProperty("is_end")
+            boolean isEnd
+    ) {
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public record Document(
+
+            String id,
+
+            @JsonProperty("place_name")
+            String placeName,
+
+            @JsonProperty("category_name")
+            String categoryName,
+
+            @JsonProperty("category_group_code")
+            String categoryGroupCode,
+
+            @JsonProperty("category_group_name")
+            String categoryGroupName,
+
+            String phone,
+
+            @JsonProperty("address_name")
+            String addressName,
+
+            @JsonProperty("road_address_name")
+            String roadAddressName,
+
+            String x,
+
+            String y,
+
+            @JsonProperty("place_url")
+            String placeUrl,
+
+            String distance
+    ) {
+    }
+}
