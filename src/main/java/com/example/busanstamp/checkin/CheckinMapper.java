8942102cc01e174a8befc515806530698b043529
KJB <drv98@naver.com>
2026-08-26 14:01:02 +0900

    4. CheckinMapper 추가

--- src/main/java/com/example/busanstamp/checkin/CheckinMapper.java
+++ src/main/java/com/example/busanstamp/checkin/CheckinMapper.java
@@ -3,6 +3,8 @@ package com.example.busanstamp.checkin;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
 @Mapper
 public interface CheckinMapper {
     // 중복확인
@@ -11,4 +13,11 @@ public interface CheckinMapper {
 
     // 새 체크인 저장
     int save(Checkin checkin);
+
+    /**
+     * 사용자의 전체 스탬프 상태
+     */
+    List<StampStatus> findStampStatusesByUserId(
+            @Param("userId") Long userId
+    );
 }
